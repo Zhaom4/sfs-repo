df0bf56abea954ec14924da33ac47a7e1b3eee9a
Zhaom4 <140111794+Zhaom4@users.noreply.github.com>
2025-09-23 14:12:30 -0500

    what does this do

--- src/pages/WelcomePage.jsx
+++ src/pages/WelcomePage.jsx
@@ -118,7 +118,7 @@ function WelcomePage() {
     
     subscriptionRef.current = subscription;
 
-    redirect('/mainpg');
+    throw redirect('/mainpg');
     // Check if user is already logged in
     const checkUser = async () => {
       if (isUnmountingRef.current) return;
