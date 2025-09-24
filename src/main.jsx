a9f17217bd09c6deb88c510ffa908315966cdf3f
Zhaom4 <140111794+Zhaom4@users.noreply.github.com>
2025-09-23 21:20:40 -0500

    fixing stuff that broke 1

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import {BrowserRouter} from "react-router-dom"
 import './index.css'
 import App from './App.jsx'
-import CustomRouter from '@/CustomRouter';
+import CustomRouter from '../../components/CustomRouter';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
