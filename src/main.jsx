9e68b29ffee8d051f659b48def26138106edbaeb
Zhaom4 <140111794+Zhaom4@users.noreply.github.com>
2025-09-23 21:27:32 -0500

    spam

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,11 @@ import { createRoot } from 'react-dom/client'
 import {BrowserRouter} from "react-router-dom"
 import './index.css'
 import App from './App.jsx'
-import CustomRouter from './components/CustomRouter';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <CustomRouter>
+    <BrowserRouter>
       <App />
-    </CustomRouter>
+    </BrowserRouter>
   </StrictMode>,
 )
