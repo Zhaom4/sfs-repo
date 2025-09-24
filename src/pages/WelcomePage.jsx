d1140e70ccaa726ac791ffe504ffe3810ba52512
Zhaom4 <140111794+Zhaom4@users.noreply.github.com>
2025-09-23 23:40:09 -0500

    TEST 2

--- src/pages/WelcomePage.jsx
+++ src/pages/WelcomePage.jsx
@@ -31,6 +31,8 @@ function WelcomePage() {
   const subscriptionRef = useRef(null);
   const isUnmountingRef = useRef(false);
 
+  return <h1>testing moremoremoremore</h1>;
+  
   if(redirect){
     return <h1>testing moremoremoremore</h1>;
   }
