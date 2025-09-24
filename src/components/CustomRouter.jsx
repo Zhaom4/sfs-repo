a88c5a965b679305215d06dcb185803185a15e89
Zhaom4 <140111794+Zhaom4@users.noreply.github.com>
2025-09-23 21:32:02 -0500

    spamming

--- src/components/CustomRouter.jsx
+++ src/components/CustomRouter.jsx
@@ -1,21 +1,32 @@
-import { Router } from "react-router";
+import { createBrowserRouter } from 'react-router-dom'
+import WelcomePage from './pages/WelcomePage'
+import MainPage from './pages/customer/MainPage'
+import { Routes, Route} from 'react-router-dom'
+import CoursePage from './pages/customer/CoursePage'
+import Favorites from './pages/customer/Favorites'
+import { useEffect, useRef } from 'react'
+import { CourseProvider } from './contexts/CourseContext'
+import MyCourses from './pages/customer/MyCourses'
+import Donations from './pages/customer/Donations'
+import { CursorProvider } from './contexts/CursorContext'
+import SignupPage from './pages/SignupPage'
+import { UserProvider } from './contexts/UserContext'
+import ProtectedRoute from './components/ProtectedRoute'
+import ResetPasswordPage from './pages/ResetPasswordPage'
+import Settings from './pages/customer/Settings'
+import Tutor from './pages/customer/Tutoring'
 
-const CustomRouter = ({ history, ...props }) => {
-  const [state, setState] = useState({
-    action: history.action,
-    location: history.location
-  });
-
-  useLayoutEffect(() => history.listen(setState), [history]);
-
-  return (
-    <Router
-      {...props}
-      location={state.location}
-      navigationType={state.action}
-      navigator={history}
-    />
-  );
-};
-
-export default CustomRouter;
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <WelcomePage />,
+  },
+  {
+    path: 'mainpg',
+    element: {
+            <ProtectedRoute>
+            <MainPage />
+            </ProtectedRoute>},
+  }
+]);
+export default router;
